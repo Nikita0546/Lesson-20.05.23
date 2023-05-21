2765e18808f2ff783e05b849f40b4f5e437ced92
Nikita0546 <116941923+Nikita0546@users.noreply.github.com>
2023-05-21 14:00:53 +0300

    Create __main__.py

---
+++ pythonProject/__main__.py
@@ -0,0 +1,15 @@
+# Создание и редактирование файлов Excel на Python
+# c пакетом openpyxl
+# https://openpyxl.readthedocs.io/en/stable/
+
+# pip install -r req.txt
+from exel import e2_read, e1_create, e3_modify, e4_styling, e5_formulas, e6_1_charts
+
+print("Hola, Azzrael Code subs!!!")
+
+e1_create.example()
+# e2_read.example()
+# e3_modify.example()
+# e4_styling.example()
+# e5_formulas.example()
+e6_1_charts.example()
