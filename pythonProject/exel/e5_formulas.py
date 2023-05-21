40f01f222f3e02f08dfb2221ba0b212edd18a9bd
Nikita0546 <116941923+Nikita0546@users.noreply.github.com>
2023-05-21 14:00:35 +0300

    Create e5_formulas.py

---
+++ pythonProject/exel/e5_formulas.py
@@ -0,0 +1,15 @@
+import openpyxl
+
+def exel():
+    """
+    Оформление таблиц (колонок, строк, ячеек)
+    :return:
+    """
+    filename="test.xlsx"
+    book = openpyxl.load_workbook(filename)
+    sheet = book.active
+
+    sheet['E2'].value = '=SUM(C1:C999)'
+    sheet['E3'].value = '=AVERAGE(C1:C999)'
+
+    book.save(filename)
